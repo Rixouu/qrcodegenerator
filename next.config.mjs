fbdf075350cf230da807effad211399c5fa14a2f
Rixouu <admin.rixou@gmail.com>
2025-04-12 23:29:32 +0900

    Fix Warning During Build Process

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
   reactStrictMode: true,
-  swcMinify: true,
   output: 'standalone',
   images: {
     domains: ['vercel.com', 'nextjs.org'],
     formats: ['image/avif', 'image/webp'],
-  },
-  experimental: {
-    // Disable experimental features that might cause issues
-    turbo: false,
   }
 };
 
